c6eac00cc4f88f972618838dd2abb8f2e312a1b5
seanoshea <Sean.OShea@betfair.com>
2014-03-02 18:42:13 +0000

    Some parsing.

---
+++ BluePlaquesLondon/src/com/upwardsnorthwards/blueplaqueslondon/Placemark.java
@@ -0,0 +1,88 @@
+/*
+ Copyright 2014 Sean O' Shea
+ 
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+ 
+ http://www.apache.org/licenses/LICENSE-2.0
+ 
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+ */
+
+package com.upwardsnorthwards.blueplaqueslondon;
+
+public class Placemark {
+
+	private String featureDescription;
+	private String name;
+	private String title;
+	private String styleUrl;
+	private double latitude;
+	private double longitude;
+	private int placemarkPinType;
+
+	public String key() {
+		return Double.toString(latitude) + Double.toString(longitude);
+	}
+
+	public String getFeatureDescription() {
+		return featureDescription;
+	}
+
+	public void setFeatureDescription(String featureDescription) {
+		this.featureDescription = featureDescription;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getStyleUrl() {
+		return styleUrl;
+	}
+
+	public void setStyleUrl(String styleUrl) {
+		this.styleUrl = styleUrl;
+	}
+
+	public double getLatitude() {
+		return latitude;
+	}
+
+	public void setLatitude(double latitude) {
+		this.latitude = latitude;
+	}
+
+	public double getLongitude() {
+		return longitude;
+	}
+
+	public void setLongitude(double longitude) {
+		this.longitude = longitude;
+	}
+
+	public int getPlacemarkPinType() {
+		return placemarkPinType;
+	}
+
+	public void setPlacemarkPinType(int placemarkPinType) {
+		this.placemarkPinType = placemarkPinType;
+	}
+}
--- BluePlaquesLondon/src/com/upwardsnorthwards/blueplaqueslondon/model/MapModel.java
+++ BluePlaquesLondon/src/com/upwardsnorthwards/blueplaqueslondon/model/MapModel.java
@@ -1,8 +1,76 @@
+/*
+ Copyright 2014 Sean O' Shea
+ 
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+ 
+ http://www.apache.org/licenses/LICENSE-2.0
+ 
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+ */
+
 package com.upwardsnorthwards.blueplaqueslondon.model;
+
+import java.util.ArrayList;
+
+import javax.xml.parsers.SAXParserFactory;
+
+import org.xml.sax.InputSource;
+import org.xml.sax.XMLReader;
+
+import android.content.Context;
+
+import com.upwardsnorthwards.blueplaqueslondon.Placemark;
+import com.upwardsnorthwards.blueplaqueslondon.utils.BluePlaquesKMLParser;
 
 public class MapModel {
 
-	public void loadMapData() {
+	private ArrayList<Placemark> placemarks = new ArrayList<Placemark>();
+	private Placemark currentPlacemark;
 
+	public void loadMapData(Context context) {
+
+		try {
+
+			XMLReader xr = SAXParserFactory.newInstance().newSAXParser()
+					.getXMLReader();
+
+			BluePlaquesKMLParser parser = new BluePlaquesKMLParser();
+
+			xr.setContentHandler(parser);
+			xr.parse(new InputSource(context.getAssets()
+					.open("blueplaques.kml")));
+
+			parser.getMapModel();
+
+		} catch (Exception e) {
+
+		}
 	}
+
+	public void addCurrentPlacemark() {
+		placemarks.add(currentPlacemark);
+	}
+
+	public ArrayList<Placemark> getPlacemarks() {
+		return placemarks;
+	}
+
+	public void setPlacemarks(ArrayList<Placemark> placemarks) {
+		this.placemarks = placemarks;
+	}
+
+	public Placemark getCurrentPlacemark() {
+		return currentPlacemark;
+	}
+
+	public void setCurrentPlacemark(Placemark currentPlacemark) {
+		this.currentPlacemark = currentPlacemark;
+	}
+
 }
