d497db62f00d6423e858dbe7084c6a1a2135ea58
seanoshea <oshea.ie@gmail.com>
2015-01-31 14:10:55 +0000

    Backing out of travis.

--- travis/script.sh
+++ travis/script.sh
@@ -1,10 +1,4 @@
 #!/bin/sh
 set -e
 cd BluePlaquesLondon
-./gradlew :build \
-          :App:testNormalDebug \
-          :App:jacocoReport \
-          :App:coveralls \
-          :App:testIntegrationDebug \
-          :App:connectedAndroidTestIntegrationDebug \
-          -PtravisCi -PdisablePreDex
+./gradlew assembleDebug
