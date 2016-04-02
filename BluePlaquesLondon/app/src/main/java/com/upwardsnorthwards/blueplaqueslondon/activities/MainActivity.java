e23144d27487b2be8b9fae0243890f8f9a415412
seanoshea <oshea.ie@gmail.com>
2016-04-02 17:56:18 -0400

    Per issue #113 - Last few bits and pieces for Android listing.

--- BluePlaquesLondon/app/src/main/java/com/upwardsnorthwards/blueplaqueslondon/activities/MainActivity.java
+++ BluePlaquesLondon/app/src/main/java/com/upwardsnorthwards/blueplaqueslondon/activities/MainActivity.java
@@ -37,7 +37,6 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v4.view.MenuItemCompat;
-import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.Menu;
