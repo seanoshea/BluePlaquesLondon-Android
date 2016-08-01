683ee6a8d610518b6be6f1e0628b2294e0355000
seanoshea <oshea.ie@gmail.com>
2016-07-31 22:02:58 -0400

    Per #146 - Trying this out.

--- BluePlaquesLondon/app/src/androidTest/java/com/upwardsnorthwards/blueplaqueslondon/utils/TestBluePlaquesKMLParser.java
+++ BluePlaquesLondon/app/src/androidTest/java/com/upwardsnorthwards/blueplaqueslondon/utils/TestBluePlaquesKMLParser.java
@@ -63,8 +63,8 @@ public class TestBluePlaquesKMLParser extends InstrumentationTestCase {
         List<Placemark> placemarks = parser.getPlacemarks();
         List<Placemark> massagedPlacemarks = parser.getMassagedPlacemarks();
 
-        assertTrue(placemarks.size() == 1021);
-        assertTrue(massagedPlacemarks.size() == 938);
+        assertTrue(placemarks.size() == 1022);
+        assertTrue(massagedPlacemarks.size() == 939);
     }
 
     public void testNames() {
